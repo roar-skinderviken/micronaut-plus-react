0961499fac098082ae07f3339900935aeb6f419a
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-02-27 11:17:03 +0100

    id("org.siouan.frontend-jdk21") version "10.0.0"

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("org.siouan.frontend-jdk21") version "9.0.0"
+    id("org.siouan.frontend-jdk21") version "10.0.0"
 }
 
 frontend {
     nodeVersion.set("20.15.0")
     assembleScript.set("run build")
-    cleanScript.set("run clean")
+    //cleanScript.set("run clean")
     checkScript.set("run check")
 }
